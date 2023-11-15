83fa08d4e59e04aa60e6979df29381c84f9f3a7c
Danil Korotenko <danil.korotenko@gmail.com>
2023-11-15 12:56:02 +0200

    server implemented.

--- server/main.cpp
+++ server/main.cpp
@@ -6,9 +6,22 @@
 //
 
 #include <iostream>
+#include "../cpp-httplib/httplib.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    httplib::Server svr;
+
+    svr.Get("/byteArray",
+        [](const httplib::Request &req, httplib::Response &res)
+        {
+            std::cout << "************" << std::endl;
+            std::cout << "byteArray: " << std::endl;
+        });
+
+    std::cout << "server ready" << std::endl;
+
+    svr.listen("0.0.0.0", 8080);
+
     return 0;
 }
